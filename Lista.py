176bc5ef02cbf9b7709fab5fd3346ae62d92be0d
Elliot-Ruiz96 <78750648+Elliot-Ruiz96@users.noreply.github.com>
2021-02-11 09:28:43 -0600

    Create Lista.py

---
+++ Lista.py
@@ -0,0 +1,15 @@
+lista = [ ]
+lista2 = [ ]
+
+print(f'Lista: {lista}')
+print(f'Lista 2: {lista2}')
+
+print(type(lista))
+
+# Agregar elementos
+#   append()
+#   insert()
+
+
+
+# Eliminar elementos
