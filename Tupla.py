7dccd5ba4b6f4a2a55ec466de07794204afffd47
Elliot-Ruiz96 <78750648+Elliot-Ruiz96@users.noreply.github.com>
2021-02-11 09:37:10 -0600

    Update Tupla.py

--- Tupla.py
+++ Tupla.py
@@ -1,3 +1,6 @@
+#En las tuplas no puede cambiar ni un solo valor despues de declararlas al principio
+#Si se usa tupla[-1] (un negativo en el indice) busca de fin a inicio
+
 tupla = 'Hola' , 2 , 3.4 , False , [ 1 , 'test' ] , 2 , 2
 tupla2 = tuple()
 
@@ -6,6 +9,7 @@ print(f'Tupla: {tupla[1]}')
 print(f'Tupla: {tupla[2]}')
 print(f'Tupla: {tupla[3]}')
 print(f'Tupla: {tupla[4][1]}')
+print(f'Tupla(-1): {tupla[-1]}')
 print(f'Tupla2: {tupla2}')
 
 print(type(tupla2))
