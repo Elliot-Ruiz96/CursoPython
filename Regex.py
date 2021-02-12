f6c8d50c29e136e6487b02d587455d715956327e
Elliot-Ruiz96 <78750648+Elliot-Ruiz96@users.noreply.github.com>
2021-02-12 11:12:53 -0600

    Create Regex.py

---
+++ Regex.py
@@ -0,0 +1,14 @@
+import re
+
+texto = 'Buenas tardes a todos y todas'
+
+patron = 'B.*t.*a'
+patron2 = 'd[aeo]s'
+
+coincidencia = re.match(patron, texto)
+
+coincidencia2 = re.search(patron, texto)
+
+encontrar=re.findall(patron2, texto)
+
+pass
