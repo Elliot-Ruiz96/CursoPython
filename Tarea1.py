7577059f170f310f11aab87d729449571966076d
Elliot-Ruiz96 <78750648+Elliot-Ruiz96@users.noreply.github.com>
2021-02-13 19:30:14 -0600

    Update Tarea1.py

--- Tarea1.py
+++ Tarea1.py
@@ -1,12 +1,17 @@
 from Pack.Tarea1_Modulo import funcion
+import random
 
 
 def main():
-    funcion()
+    funcion(s_user, s_pc)
 
 
 if __name__ == '__main__':
     print('Bienvenido al juego de piedra, papel o tijera')
     print('Escribe tu eleccion: ')
+    s_user = input()
+    s_pc = random.choice(['piedra', 'papel', 'tijera'])
+    print('Eleccion de usuario: ',s_user)
+    print('Eleccion de PC: ', s_pc)
     main()
 
