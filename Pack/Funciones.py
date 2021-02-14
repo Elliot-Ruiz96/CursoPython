1c998deb0780b68b0809d9920237afc964a282ee
Elliot-Ruiz96 <78750648+Elliot-Ruiz96@users.noreply.github.com>
2021-02-13 19:13:40 -0600

    Update Funciones.py

--- Pack/Funciones.py
+++ Pack/Funciones.py
@@ -4,8 +4,8 @@ def funcion( nombre = 'Elliot', apellido = 'Ruiz', lista=['a','b','c','d']):
     print('Hello', nombre, apellido)
     print(f'lista:  {lista}')
     lista[1] = 14
-
     return lista
+
 
 if __name__ == '__main__':
     lista = [ 1 , 2 , 3]
