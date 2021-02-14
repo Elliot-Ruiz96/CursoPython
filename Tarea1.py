6b60bd94f1f93deff7d2787cc51908476d1327b0
Elliot-Ruiz96 <78750648+Elliot-Ruiz96@users.noreply.github.com>
2021-02-13 19:13:44 -0600

    Update Tarea1.py

--- Tarea1.py
+++ Tarea1.py
@@ -1,32 +1,12 @@
-#Modulo para seleccion aleatoria de PC
-#import numpy as np
+from Pack.Tarea1_Modulo import funcion
 
-print('Bienvenido al juego de piedra, papel o tijera')
-print('Escribe tu eleccion: ')
 
-#Seleccion de opciones usuario y PC
-seleccion_usuario = input()
-print('Escribe tu eleccion2: ')
-seleccion_pc = input()
-#seleccion_pc = np.random.choice(['piedra','papel','tijera'])
+def main():
+    funcion()
 
-if seleccion_usuario != seleccion_pc:
-    if seleccion_usuario == 'piedra':
-        if seleccion_pc == 'papel':
-            print('Perdiste!')
-        else:
-            print('Ganaste!')
-    elif seleccion_usuario == 'papel':
-        if seleccion_pc == 'tijera':
-            print('Perdiste!')
-        else:
-            print('Ganaste!')
-    elif seleccion_usuario == 'tijera':
-        if seleccion_pc == 'piedra':
-            print('Perdiste!')
-        else:
-            print('Ganaste!')
-    else:
-        print('Seleccion no valida!')
-else:
-    print('Empate!')
+
+if __name__ == '__main__':
+    print('Bienvenido al juego de piedra, papel o tijera')
+    print('Escribe tu eleccion: ')
+    main()
+
