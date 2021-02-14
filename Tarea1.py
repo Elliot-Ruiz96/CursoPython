7ab653a63979ae28b56a393d96657f0941e4fc28
Elliot-Ruiz96 <78750648+Elliot-Ruiz96@users.noreply.github.com>
2021-02-13 19:33:04 -0600

    Update Tarea1.py

--- Tarea1.py
+++ Tarea1.py
@@ -6,12 +6,11 @@ def main():
     funcion(s_user, s_pc)
 
 
-if __name__ == '__main__':
-    print('Bienvenido al juego de piedra, papel o tijera')
-    print('Escribe tu eleccion: ')
-    s_user = input()
-    s_pc = random.choice(['piedra', 'papel', 'tijera'])
-    print('Eleccion de usuario: ',s_user)
-    print('Eleccion de PC: ', s_pc)
-    main()
+print('Bienvenido al juego de piedra, papel o tijera')
+print('Escribe tu eleccion: ')
+s_user = input()
+s_pc = random.choice(['piedra', 'papel', 'tijera'])
+print('Eleccion de usuario: ',s_user)
+print('Eleccion de PC: ', s_pc)
+main()
 
