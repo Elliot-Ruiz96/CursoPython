6713ba997bfc044a6d5097498a7f9cebe6466967
Elliot-Ruiz96 <78750648+Elliot-Ruiz96@users.noreply.github.com>
2021-02-13 19:30:11 -0600

    Update Tarea1_Modulo.py

--- Pack/Tarea1_Modulo.py
+++ Pack/Tarea1_Modulo.py
@@ -1,47 +1,31 @@
 #import random
+#s_user = input()
 #s_pc = random.choice(['piedra', 'papel', 'tijera'])
+#s_user = 'piedra', s_pc = 'tijera'
 
 
-def funcion(s_user = 'piedra', s_pc = 'tijera'):
+def funcion(s_user, s_pc):
 
     if s_user != s_pc:
         if s_user == 'piedra':
             if s_pc == 'papel':
-                resultado = 'Perdiste!'
-                print(resultado)
-                # print('Perdiste!')
+                print('Perdiste!')
             else:
-                resultado = 'Ganaste!'
-                print(resultado)
-                # print('Ganaste!')
+                print('Ganaste!')
         elif s_user == 'papel':
             if s_pc == 'tijera':
-                resultado = 'Perdiste!'
-                print(resultado)
-                # print('Perdiste!')
+                print('Perdiste!')
             else:
-                resultado = 'Ganaste!'
-                print(resultado)
-                # print('Ganaste!')
+                print('Ganaste!')
         elif s_user == 'tijera':
             if s_pc == 'piedra':
-                resultado = 'Perdiste!'
-                print(resultado)
-                # print('Perdiste!')
+                print('Perdiste!')
             else:
-                resultado = 'Ganaste!'
-                print(resultado)
-                # print('Ganaste!')
+                print('Ganaste!')
         else:
-            resultado = 'Seleccion no valida!'
-            print(resultado)
-            # print('Seleccion no valida!')
+            print('Seleccion no valida!')
     else:
-        resultado = 'Empataste!'
-        print(resultado)
-        # print('Empate!')
-
-    return resultado
+        print('Empate!')
 
 
 if __name__ == '__main__':
