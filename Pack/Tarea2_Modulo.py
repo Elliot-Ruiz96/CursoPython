6ab01a2e406c12ab8af0f0fc794a11244ff1fdb5
Elliot-Ruiz96 <78750648+Elliot-Ruiz96@users.noreply.github.com>
2021-02-16 22:18:52 -0600

    Update Tarea2_Modulo.py

--- Pack/Tarea2_Modulo.py
+++ Pack/Tarea2_Modulo.py
@@ -1,13 +1,16 @@
 import re
 
 
-def validacion(correo, telefono):
+def validacion(correo, telefono, curp):
 
     # Variable para validacion de correo
     valida_correo = re.search("@", correo)
     valida_correo_dominio = re.split("@", correo)
     # Variable para validacion de telefono
     valida_telefono = re.findall("\d", telefono)
+    # Variable para validacion de curp
+    valida_curp0 = re.split("\d",curp)
+    # Variable para validacion de rfc
 
     if valida_correo:
         print("Yes")
@@ -21,6 +24,12 @@ def validacion(correo, telefono):
     else:
         print(f'Numero {telefono} no valido.')
 
+    if len(curp) == 18:
+        print("Yes")
+    else:
+        print(f'CURP {curp} no valida.')
+
+
 
 if __name__ == '__main__':
-    validacion("jj@gmail.com", "4775531264")
+    validacion("jj@gmail.com", "4775531264", "RUSE960823HGTZNL03")
