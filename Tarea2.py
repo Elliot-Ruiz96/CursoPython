56868e8d47284f127813e42d84ed393ed0ea572c
Elliot-Ruiz96 <78750648+Elliot-Ruiz96@users.noreply.github.com>
2021-02-16 21:58:31 -0600

    Create Tarea2.py

---
+++ Tarea2.py
@@ -0,0 +1,10 @@
+import re
+
+print("Ingresa tu email: \n")
+correo = input()
+print("Ingresa tu numero celular: \n")
+numero = input()
+print("Ingresa tu RCF: \n")
+rfc = input()
+print("Ingresa tu CURP: \n")
+curp = input()
