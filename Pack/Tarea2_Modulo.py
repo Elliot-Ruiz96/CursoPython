1c49a6f61a13cefe8e3987f8ee6665d6b8c5b708
RedopVago <juan.valenzuela@cinvestav.mx>
2021-02-22 00:59:06 -0600

    Comentarios

--- Pack/Tarea2_Modulo.py
+++ Pack/Tarea2_Modulo.py
@@ -4,13 +4,24 @@ import re
 def validacion(correo, telefono, curp, rfc):
 
     # Variable para validacion de correo
+    # [PV] # La validacion es muy debil, ingresar solo @. lo marca como valida
+    # patron sencillo '[a-z.]+@([a-z.]+){1,2}[a-z]{2-3}'
     valida_correo = re.search("@", correo)          # Busca el arroba
     valida_correo_dominio = re.split("@", correo)   # Divide en dos grupos el correo
+
     # Variable para validacion de telefono
+    # [PV] Busca que haya 10 numeros pero no valida la forma
+    # [PV] ej. '\([0-9]{3}\) [0-9]{3}-[0-9]{4}'
     valida_telefono = re.findall("\d", telefono)    # Verifica que sean solo numeros
+
     # Variable para validacion de curp
+    # [PV] Si se escriben puros numeros se toma com valido
+    # ej. valido [A-Z]{4}[0-9]{6}[H|M][A-Z]{5}[A-Z0-9]{2}
     valida_curp = re.search("\S", curp)             # Busca que no haya espacios
+
     # Variable para validacion de rfc
+    # [PV] Si se escriben puros numeros se toma com valido
+    # ej. valido [A-Z]{4}[0-9]{6}[A-Z0-9]{3}
     valida_rfc = re.search("\S", rfc)  # Busca que no haya espacios
 
     if valida_correo:
