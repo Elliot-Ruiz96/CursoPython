c62f16fba3b277602910fa061dd77a95323b0e96
Elliot-Ruiz96 <78750648+Elliot-Ruiz96@users.noreply.github.com>
2021-02-23 10:01:03 -0600

    Create server.py

---
+++ server.py
@@ -0,0 +1,23 @@
+import pickle
+
+s = 'Hola mundo'
+
+print(s)
+print(type(s))
+
+se = s.encode()
+
+print(se)
+print(type(se))
+
+sp = pickle.dumps(s)
+
+print(sp)
+print(type(sp))
+
+ss2 = pickle.loads(se)
+
+print(ss2)
+print(type(ss2))
+
+# No imprime debido a que encuenra una h primero en lugar de la direccion \x80
