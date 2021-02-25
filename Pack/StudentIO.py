73b2a2f297c93665f9d724a61ee4a582a106f9a0
Elliot-Ruiz96 <78750648+Elliot-Ruiz96@users.noreply.github.com>
2021-02-24 20:23:27 -0600

    Update StudentIO.py

--- Pack/StudentIO.py
+++ Pack/StudentIO.py
@@ -1,11 +1,14 @@
+import pickle
+
+
 class Estudiante:
 
-    def __init__(self):
-        self.nombre = "Elliot Ruiz"
-        self.carrera = "Mecatronica"
-        self.correo = "elliotruizs@iee.org"
-        self.num_control = "16240056"
-        self.promedio = "82.47"
+    def __init__(self, nombre, carrera, correo, num_control, promedio):
+        self.nombre = nombre
+        self.carrera = carrera
+        self.correo = correo
+        self.num_control = num_control
+        self.promedio = promedio
 
     def setnombre(self):
         nombre = input()
@@ -43,7 +46,12 @@ class Estudiante:
         return self.promedio
 
 
-e = Estudiante()
+# e = Estudiante(nombre, carrera, correo, num_control, promedio)
+e = Estudiante("Elliot", "MECATRONICA", "elliotruizs@iee.org", "16240056", "82.47")
+e1 = Estudiante("DIEGO", "ACTUARIA", "diegovo@iee.org", "16240057", "83.47")
+e2 = Estudiante("RENE", "SISTEMAS", "reneva@iee.org", "16240058", "84.47")
+e3 = Estudiante("SERGIO", "GESTION", "sergiorv@iee.org", "16240059", "85.47")
+e4 = Estudiante("KARLA", "ADMINISTRACION", "karlahe@iee.org", "16240060", "86.47")
 
 
 def agregar():
@@ -83,3 +91,22 @@ def actualizar():
     e.setnum_control()
     print("Ingresa tu promedio: ")
     e.setpromedio()
+
+
+def pickle1():
+    ej_dict = {1: e.nombre, 2: e.carrera, 3: e.correo, 4: e.num_control, 5: e.promedio}
+    ej_dict1 = {1: e1.nombre, 2: e1.carrera, 3: e1.correo, 4: e1.num_control, 5: e1.promedio}
+    ej_dict2 = {1: e2.nombre, 2: e2.carrera, 3: e2.correo, 4: e2.num_control, 5: e2.promedio}
+    ej_dict3 = {1: e3.nombre, 2: e3.carrera, 3: e3.correo, 4: e3.num_control, 5: e3.promedio}
+    ej_dict4 = {1: e4.nombre, 2: e4.carrera, 3: e4.correo, 4: e4.num_control, 5: e4.promedio}
+    pickle_out = open("dict.db", "wb")
+    pickle.dump(ej_dict, pickle_out)
+    pickle.dump(ej_dict1, pickle_out)
+    pickle.dump(ej_dict2, pickle_out)
+    pickle.dump(ej_dict3, pickle_out)
+    pickle.dump(ej_dict4, pickle_out)
+    pickle_out.close()
+    pickle_in = open("dict.db", "rb")
+    example_dict = pickle.load(pickle_in)
+    print(example_dict)
+    print(example_dict[2])
