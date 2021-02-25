ef94867664ea9c9422e42b82ba188e9649089e0b
Elliot-Ruiz96 <78750648+Elliot-Ruiz96@users.noreply.github.com>
2021-02-24 20:23:39 -0600

    Update Tarea3.py

--- Tarea3.py
+++ Tarea3.py
@@ -1,24 +1,28 @@
 from Pack.StudentIO import agregar
 from Pack.StudentIO import lectura
 from Pack.StudentIO import actualizar
+from Pack.StudentIO import pickle1
 
 
 def menu():
-    choice = input()
-    if choice == '1':
-        agregar()
-    elif choice == '2':
-        lectura()
-    elif choice == '3':
-        actualizar()
-    else:
-        print("Eleccion no valida. ")
+    while True:
+        print("Bienvenido a la tarea 3.")
+        print("1. Agregar nuevo alumno.")
+        print("2. Lectura de alumno.")
+        print("3. Actualizar alumno.")
+        print("4. Salir del programa")
+        print("Ingrese su eleccion: ")
+        choice = input()
+        if choice == '1':
+            agregar()
+        elif choice == '2':
+            lectura()
+            pickle1()
+        elif choice == '3':
+            actualizar()
+        elif choice == '4':
+            print("Adios!")
+            break
 
 
-print("Bienvenido a la tarea 3.")
-print("1. Agregar nuevo alumno.")
-print("2. Lectura de alumno.")
-print("3. Actualizar alumno.")
-print("4. Salir del programa")
-print("Ingrese su eleccion: ")
 menu()
