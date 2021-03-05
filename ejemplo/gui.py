6b6cdb939e82f919b255a1485912ede02d33a52d
Elliot-Ruiz96 <78750648+Elliot-Ruiz96@users.noreply.github.com>
2021-03-04 20:56:26 -0600

    Update gui.py

--- ejemplo/gui.py
+++ ejemplo/gui.py
@@ -1,15 +1,15 @@
 from PySide2.QtWidgets import QApplication
 from PySide2.QtWidgets import QMainWindow
 import sys
-from main import Ui_Form
+from main import Ejemplo
 
 # Windows -> desinger.exe
 
 # Inicializacon de gui
 app = QApplication(sys.argv)
 # Inicializacion de ventana principal
-# window = QMainWindow()
-window = Ui_Form()
+window = QMainWindow()
+# window = Ejemplo()
 # Muestra la ventana creada
 window.show()
 # Ejecucion de gui
