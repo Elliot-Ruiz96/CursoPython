270db4969fa9b96ad60eb68471e7588aa9ab10a7
Elliot-Ruiz96 <78750648+Elliot-Ruiz96@users.noreply.github.com>
2021-03-04 20:56:23 -0600

    Update main.py

--- ejemplo/main.py
+++ ejemplo/main.py
@@ -8,22 +8,30 @@ from PySide2.QtCore import QFile
 from PySide2.QtUiTools import QUiLoader
 
 
-class Ui_Form(QWidget):
+class Ejemplo(QWidget):
     def __init__(self):
-        super(Ui_Form, self).__init__()
+        super(Ejemplo, self).__init__()
         self.load_ui()
+
+        #self.pushButton.clicked.connect(slot1)
 
     def load_ui(self):
         loader = QUiLoader()
-        path = os.path.join(os.path.dirname(__file__), "form.ui")
+        path = os.path.join(os.path.dirname(__file__), "ejemplo.ui")
         ui_file = QFile(path)
         ui_file.open(QFile.ReadOnly)
         loader.load(ui_file, self)
         ui_file.close()
 
+    def slot1(self):
+        print('Boton presionado!')
+
+    def fun1 (self):
+        pass
+
 
 if __name__ == "__main__":
     app = QApplication([])
-    widget = Ui_Form()
+    widget = Ejemplo()
     widget.show()
     sys.exit(app.exec_())
