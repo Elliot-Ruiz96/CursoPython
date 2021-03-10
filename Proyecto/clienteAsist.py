4c5772b129d826f51419df41a0822f8353c44ce7
Elliot-Ruiz96 <78750648+Elliot-Ruiz96@users.noreply.github.com>
2021-03-10 17:50:36 -0600

    Update clienteAsist.py

--- Proyecto/clienteAsist.py
+++ Proyecto/clienteAsist.py
@@ -1,47 +1,33 @@
 from PySide6 import QtWidgets
-from PySide2 import QtWidgets
 from estudiante import Estudiante
 import sys
 import socket
 import pickle
 
+host = '3.16.226.150'
+port = 9998
 
-# Clase padre para el menu
+
 class Menu(QtWidgets.QWidget):
-    ModoNavegar, ModoIngresar, ModoEditar = range(3)
 
-    # Funcion para declarar e inicializar los widgets
     def __init__(self, parent=None):
         super(Menu, self).__init__(parent)
 
-        # Se realiza la codificacion en lugar del archivo ui
-
         nameLabel1 = QtWidgets.QLabel("Nombre:")
         self.nameLine = QtWidgets.QLineEdit()
-
         nameLabel2 = QtWidgets.QLabel("Correo:")
         self.mailLine = QtWidgets.QLineEdit()
-
         nameLabel3 = QtWidgets.QLabel("Contraseña:")
         self.passLine = QtWidgets.QLineEdit()
 
-        # Botones para funcion de menu
-        self.submitButton = QtWidgets.QPushButton("&Confirma")
-        # Cambiar para archivo
-        self.codeButton = QtWidgets.QPushButton("&Buscar...")
-        self.codeButton.setToolTip("Cargar archivo .zip")
+        self.submitButton = QtWidgets.QPushButton("&Buscar y enviar")
+        self.submitButton.setToolTip("Cargar archivo .zip")
 
-        # Definir la conexion a funciones
         self.submitButton.clicked.connect(self.submitAlumno)
-        self.codeButton.clicked.connect(self.buscarCodigo)
 
-        # Layout de funciones principales
         buttonLayout1 = QtWidgets.QVBoxLayout()
         buttonLayout1.addWidget(self.submitButton)
-        buttonLayout1.addWidget(self.codeButton)
-        buttonLayout1.addStretch()
 
-        # Layout principal con coordenadas
         mainLayout = QtWidgets.QGridLayout()
         mainLayout.addWidget(nameLabel1, 0, 0)
         mainLayout.addWidget(self.nameLine, 0, 1)
@@ -54,36 +40,22 @@ class Menu(QtWidgets.QWidget):
         self.setLayout(mainLayout)
         self.setWindowTitle("Proyecto")
 
-    def submitAlumno(self, fileName):
-        # Agregar clase estudiante
+    def submitAlumno(self):
+
         s = socket.socket()
-
-# Port = 9997 proyecto final
-# Port = 9998 pruebas
-
-        host = '3.16.226.150'
-        port = 9998
-
+        # Port = 9997 proyecto final, 9998 pruebas
         s.connect((host, port))
 
         estudiante = Estudiante(self.nameLine.text(), self.mailLine.text(), self.passLine.text())
-
         estudiante_seriado = pickle.dumps(estudiante)
 
         s.send(estudiante_seriado)
-
         res = s.recv(1024)
         print(f'Respuesta: \n\t{res.decode()}')
 
-        s.close()
-
-    def buscarCodigo(self):
-        s = socket.socket()
-
-        host = '3.16.226.150'
-        port = 9998
-
-        s.connect((host, port))
+        s.send(b'INI')
+        res = s.recv(1024)
+        print(f'Respuesta: \n\t{res.decode()}')
 
         fileName, _ = QtWidgets.QFileDialog.getOpenFileName(self,
                                                             "Open ZIP file",
@@ -94,13 +66,29 @@ class Menu(QtWidgets.QWidget):
 
         fileName_seriado = pickle.dumps(fileName)
 
-        s.send(fileName_seriado)
+        i = True
+        j = 0
+
+        while i:
+            chunk = fileName_seriado[j: i + 1024]
+
+            if not chunk:
+                break
+
+            s.send(chunk)
+            res = s.recv(1024)
+            print(f'Respuesta: \n\t{res.decode()}')
+            j += 1024
+
+        s.send(b'FIN')
+        res = s.recv(1024)
+        print(f'Respuesta: \n\t{res.decode()}')
 
         res = s.recv(1024)
         print(f'Respuesta: \n\t{res.decode()}')
 
         s.close()
-        
+
 
 if __name__ == '__main__':
 
